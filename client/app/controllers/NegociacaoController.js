4883053ac01206f92ebffa28efeef8e31b40dfbb
elvinciqueira <elvinciqueira@gmail.com>
2021-05-27 21:20:51 -0300

    capitulo 9

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -5,8 +5,23 @@ class NegociacaoController {
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
 
-    this._negociacoes = new Negociacoes((model) => {
-      this._negociacoesView.update(model);
+    const self = this;
+
+    this._negociacoes = new Proxy(new Negociacoes(), {
+      get(target, prop, receiver) {
+        if (
+          typeof target[prop] == typeof Function &&
+          ['adiciona', 'esvazia'].includes(prop)
+        ) {
+          return function () {
+            target[prop].apply(target, arguments);
+
+            self._negociacoesView.update(target);
+          };
+        } else {
+          return target[prop];
+        }
+      },
     });
 
     this._negociacoesView = new NegociacoesView('#negociacoes');
--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -1,15 +1,11 @@
 class Negociacoes {
-  constructor(armadilha) {
+  constructor() {
     this._negociacoes = [];
-    this._armadilha = armadilha;
-
     Object.freeze(this);
   }
 
   adiciona(negociacao) {
     this._negociacoes.push(negociacao);
-
-    this._armadilha(this);
   }
 
   paraArray() {
@@ -26,6 +22,5 @@ class Negociacoes {
 
   esvazia() {
     this._negociacoes.length = 0;
-    this._armadilha(this);
   }
 }
